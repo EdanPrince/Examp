dd8f5b374097c544927fc7b8456446b1e4283cab
EdanPrince <112059492+EdanPrince@users.noreply.github.com>
2023-03-03 09:19:35 +0700

    Add files via upload

---
+++ ThiThucHanh/Examp2/TestFlight.java
@@ -0,0 +1,8 @@
+public class TestFlight {
+    public static void main(String[] args) {
+        Flight g1 = new Flight(857, "Toronto");
+        System.out.println(g1.getNumber());
+        System.out.println(g1.getDestination());
+        g1.display();
+    }
+}
